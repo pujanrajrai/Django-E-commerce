914b3d9694678a1402e1433b5b9a77c82677d532
Pujan Raj Rai <pujanr0809@gmail.com>
2020-08-30 18:57:58 +0545

    migration is made and super user is create

--- shops/models.py
+++ shops/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Products(models.Model):
+    product_id= models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
